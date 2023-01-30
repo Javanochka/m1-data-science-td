9ad533963fe5a2e0a0039761fb44b53e48e3b566
Pierre Epron <pierre.epron@gmail.com>
2023-01-30 15:36:42 +0100

    add scrapy solution

--- class02-scraping/Exercise_sheet_3_scrapy.py
+++ class02-scraping/Exercise_sheet_3_scrapy.py
@@ -1,23 +1,28 @@
+from pathlib import Path
 import scrapy
 import json
 
-class WikipediaSpider: #Something is missing here. What exactly?
+class WikipediaSpider(scrapy.Spider): #Something is missing here. What exactly?
     name = "wikipedia"
 
     start_urls = ["https://en.wikipedia.org/wiki/List_of_French_artists"]
 
     def parse(self, response):
-        list_els = #get all links which are in lists on the page
-        list_see_also = #get the links in see also (they are not about the disctinct authors)
+        #get all links which are in lists on the page
+        list_els = response.xpath('//div[@class="mw-parser-output"]/ul/li/a[1]/@href').getall()
+        #get the links in see also (they are not about the disctinct authors)
+        list_see_also = response.xpath('//h2[span/@id="See_also"]/following-sibling::ul/li/a/@href').getall()
         res_list = list(set(list_els) - set(list_see_also))
         for link in res_list:
             #check that the link actually exists and is not red
+            if not link.endswith('&redlink=1'):
                 yield response.follow(link, callback=self.parse_artist)
         
     def parse_artist(self, response):
-        url = #get url of the page
-        name = # get name of the artist
-        paragraph = # get the first paragraph
+        url = response.url #get url of the page
+        name = response.xpath('//h1/span/text()').get().split('(')[0].strip() # get name of the artist
+        # [@class != "mw-empty-elt"]
+        paragraph = (''.join(response.xpath('//div[@class="mw-parser-output"]/p[not(@class)][1]//text()').getall())).strip() # get the first paragraph
         yield {'url': url,
                'name': name,
                'paragraph': paragraph}
@@ -25,6 +30,9 @@ class WikipediaSpider: #Something is missing here. What exactly?
         
 if __name__=='__main__':
     import scrapy.crawler
+
+    if Path('artists.json').is_file():
+        Path('artists.json').unlink()
     
     process = scrapy.crawler.CrawlerProcess({
         'USER_AGENT': 'Mozilla/5.0',
