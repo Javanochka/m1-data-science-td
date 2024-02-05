e6f3a7b0fd0616c6038c8c7dddbfa0c42450ae77
Hanady <hanady02yasmine@gmail.com>
2024-02-05 17:15:20 +0100

    My changes

--- class01-git/facts.py
+++ class01-git/facts.py
@@ -11,6 +11,8 @@ people = [
            facts=['I can meow like a cat', 'I am a PhD student', 'I am not Estonian']),
     Person(github_login="cat", first_name="Martha", last_name="KittyCat",
            facts=['I am a dog',"I can say meow", "I like milk"]),
+    Person(github_login='hanadyyasmine', first_name='Hanady', last_name='Yasmine',
+           facts=['I am in M1 NLP class', 'I live in Paris', 'I am a data scientist']),
 ]
 #facts are numerated from 0
 answers = {'example': 1,
