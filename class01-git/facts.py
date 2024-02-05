af90137de6a48ea3ebbaa54dcff937a9f4c6c45e
Abigail <abigail.berthe@gmail.com>
2024-02-05 16:53:00 +0100

    resolve conflict

--- class01-git/facts.py
+++ class01-git/facts.py
@@ -11,6 +11,8 @@ people = [
            facts=['I can meow like a cat', 'I am a PhD student', 'I am not Estonian']),
     Person(github_login="cat", first_name="Martha", last_name="KittyCat",
            facts=['I am a dog',"I can say meow", "I like milk"]),
+    Person(github_login="robin", first_name="Tweety", last_name="Bird",
+           facts=["I have arms", "I can fly", "I eat worms"]),
     Person(github_login="AbigailBerthe", first_name="Abigail", last_name="Berthe",
            facts=['I am a cat',"I can talk", "I like chocolate"]),
 ]
