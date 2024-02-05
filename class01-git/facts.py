1d40f39e83e6c162317eca40bcf16970cb103834
Hawawou <hawawout2000@gmail.com>
2024-02-05 15:58:42 +0100

    added my facts

--- class01-git/facts.py
+++ class01-git/facts.py
@@ -11,6 +11,7 @@ people = [
            facts=['I can meow like a cat', 'I am a PhD student', 'I am not Estonian']),
     Person(github_login="cat", first_name="Martha", last_name="KittyCat",
            facts=['I am a dog',"I can say meow", "I like milk"]),
+    Person(github_login="Hawawou", first_name="Hawawou", facts=['I am a student', 'I am from Togo', 'I love cats'])
 ]
 #facts are numerated from 0
 answers = {'example': 1,
