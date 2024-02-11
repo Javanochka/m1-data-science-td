722989d3ae5e9d012ca439e7389cee52d5cbf52c
AmelieKnecht <aknecht@kabelmail.de>
2024-02-11 11:04:31 +0100

    could not solve it all

--- class02-scraping/Exercise_sheet_3_scrapy.py
+++ class02-scraping/Exercise_sheet_3_scrapy.py
@@ -1,22 +1,26 @@
 import scrapy
 import json
 
-class WikipediaSpider: #Something is missing here. What exactly?
+class WikipediaSpider(scrapy.Spider): 
     name = "wikipedia"
 
     start_urls = ["https://en.wikipedia.org/wiki/List_of_French_artists"]
 
     def parse(self, response):
-        list_els = #get all links which are in lists on the page
-        list_see_also = #get the links in see also (they are not about the disctinct authors)
+        list_els = response.css('#mw-content-text div[role="note"] + ul li a::attr(href)').getall()
+        list_see_also = response.css('h2:has(\#See_also) + ul li a::attr(href)').getall()
         res_list = list(set(list_els) - set(list_see_also))
         for link in res_list:
             #check that the link actually exists and is not red
-                yield response.follow(link, callback=self.parse_artist)
+            yield response.follow(link, callback=self.parse_artist)
         
     def parse_artist(self, response):
-        url = #get url of the page
-        name = # get name of the artist
+        base_url = 'https://en.wikipedia.org'
+        url = response.css('#mw-content-text div[role="note"] + ul li a::attr(href)').getall()
+        for link in url:
+            link = base_url+link
+            print(link)
+        name = response.css('#mw-content-text div[role="note"] + ul li a::attr(title)').getall()
         paragraph = # get the first paragraph
         yield {'url': url,
                'name': name,
@@ -27,7 +31,7 @@ if __name__=='__main__':
     import scrapy.crawler
     
     process = scrapy.crawler.CrawlerProcess({
-        'USER_AGENT': 'Mozilla/5.0',
+        'USER_AGENT': 'Safari',
         'FEEDS': {
             "artists.json": {"format": "json"},
         },
